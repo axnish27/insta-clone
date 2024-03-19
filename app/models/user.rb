6384bc73027d4935d1148c66d6d8e6156bbb413f
axnish27 <itsaanish10@gmail.com>
2024-03-20 01:20:32 +0530

    integrated devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+
+  validates :username, presence: true ,length: { minimum: 3, maximum: 25 } , uniqueness: true
+
 end
