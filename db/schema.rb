74f24095831ac103d0b17de4f581138adb2d45b4
axnish27 <itsaanish10@gmail.com>
2024-03-22 05:44:37 +0530

    follower changed to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_20_191430) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_21_235515) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,10 +23,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_20_191430) do
   end
 
   create_table "follows", force: :cascade do |t|
-    t.integer "follower_id"
+    t.integer "user_id"
     t.integer "followee_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "likes", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,53 +1,52 @@
-
-users = []
-20.times do
-  user = User.create!(
-    name: Faker::Name.name,
-    username: Faker::Alphanumeric.alphanumeric(number: 8, min_alpha: 5),
-    dob: Faker::Date.birthday(min_age: 18, max_age: 65).strftime("%Y-%m-%d"),
-    about: Faker::Emotion.adjective,
-    email: Faker::Internet.email(domain: 'gmail.com'),
-    password: Faker::Lorem.characters(number: 10)
-  )
-  users << user
-end
+# users = []
+# 20.times do
+#   user = User.create!(
+#     name: Faker::Name.name,
+#     username: Faker::Alphanumeric.alphanumeric(number: 8, min_alpha: 5),
+#     dob: Faker::Date.birthday(min_age: 18, max_age: 65).strftime("%Y-%m-%d"),
+#     about: Faker::Emotion.adjective,
+#     email: Faker::Internet.email(domain: 'gmail.com'),
+#     password: Faker::Lorem.characters(number: 10)
+#   )
+#   users << user
+# end
 #p users
 
-users.each do |user|
-  5.times do
-      post = Post.create!(
-        content: Faker::Quote.famous_last_words,
-        caption: Faker::Fantasy::Tolkien.character,
-        user_id: user.id
-      )
-      10.times do
-        like = Like.create!(
-          follower_id: Faker::Number.between(from: 1, to: 22), # Select a random user_id from the array
-          post_id: post.id
-        )
-      end
-      10.times do
-        comment = Comment.create!(
-          follower_id: Faker::Number.between(from: 1, to: 22), # Select a random user_id from the array
-          post_id: post.id,
-          comment_text: Faker::Emotion.noun
-        )
-      end
-  end
-end
+# users.each do |user|
+#   5.times do
+#       post = Post.create!(
+#         content: Faker::Quote.famous_last_words,
+#         caption: Faker::Fantasy::Tolkien.character,
+#         user_id: user.id
+#       )
+#       10.times do
+#         like = Like.create!(
+#           follower_id: Faker::Number.between(from: 1, to: 22), # Select a random user_id from the array
+#           post_id: post.id
+#         )
+#       end
+#       10.times do
+#         comment = Comment.create!(
+#           follower_id: Faker::Number.between(from: 1, to: 22), # Select a random user_id from the array
+#           post_id: post.id,
+#           comment_text: Faker::Emotion.noun
+#         )
+#       end
+#   end
+# end
 
 
 50.times do
-  follower_id = rand(1..22)
+  user_id = rand(1..22)
   followee_id = rand(1..22)
 
-  # Ensure follower_id and followee_id are different
-  while follower_id == followee_id
+  # Ensure user_id and followee_id are different
+  while user_id == followee_id
     followee_id = rand(1..22)
   end
 
   follow = Follow.create!(
-    follower_id: follower_id,
+    user_id: user_id,
     followee_id: followee_id
   )
 end
